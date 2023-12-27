c529320b75aa01739a87451483b90b7cf8ffc42c
Ikey168 <122488906+Ikey168@users.noreply.github.com>
2023-12-27 21:38:49 +0100

    updated article_list to get full text + full_list

--- src/collector.py
+++ src/collector.py
@@ -12,12 +12,17 @@ import newspaper
 class Collector:
     def __init__(self):
         self.article_urls = []
+        self.article_titles = []
+        self.article_texts = []
     
     def article_list(self, source_url):
-        cnn_paper = newspaper.build(source_url)
-        for article in cnn_paper.articles:
+        paper = newspaper.build(source_url)
+        for article in paper.articles:
             self.article_urls.append(article.url)
+            self.article_titles.append(article.title)
+            self.article_texts.append(article.text)
 
     def full_list(self, source_urls):
         for source_url in source_urls:
             self.article_list(source_url)
+
