cdc253be6fb0d3e3b99654f10873314117b17cb2
Ikey168 <122488906+Ikey168@users.noreply.github.com>
2023-12-27 20:19:51 +0100

    added function full_list

--- src/collector.py
+++ src/collector.py
@@ -13,8 +13,11 @@ class Collector:
     def __init__(self):
         self.article_urls = []
     
-    def article_list(self, news_website):
-        cnn_paper = newspaper.build(news_website)
+    def article_list(self, source_url):
+        cnn_paper = newspaper.build(source_url)
         for article in cnn_paper.articles:
             self.article_urls.append(article.url)
-            print(article.url)
+
+    def full_list(self, source_urls):
+        for source_url in source_urls:
+            self.article_list(source_url)
