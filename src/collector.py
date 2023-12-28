b10f7ec377666e09444a91f9d0f2614741ed802b
Ikey168 <122488906+Ikey168@users.noreply.github.com>
2023-12-28 04:13:32 +0100

    added delete to MediaDB

--- src/collector.py
+++ src/collector.py
@@ -32,7 +32,17 @@ class MediaDB:
         return db
 
     def insert(self, table_name, data):
-        """ Connect to the PostgreSQL database server """
+        """
+        Insert data into a PostgreSQL table.
+
+        Parameters:
+        - connection_params (dict): Dictionary containing connection parameters (e.g., {'user': 'your_user', 'password': 'your_password', 'host': 'your_host', 'database': 'your_database'}).
+        - table_name (str): Name of the PostgreSQL table.
+        - data (dict): Dictionary containing column names and corresponding values.
+
+        Returns:
+        - None
+        """
         conn = None
         try:
             # read connection parameters
@@ -73,6 +83,53 @@ class MediaDB:
                 cur.close()
             if conn:
                 conn.close()
+
+    def delete(self, table_name, condition):
+        """
+        Delete data from a PostgreSQL table based on a specified condition.
+
+        Parameters:
+        - connection_params (dict): Dictionary containing connection parameters (e.g., {'user': 'your_user', 'password': 'your_password', 'host': 'your_host', 'database': 'your_database'}).
+        - table_name (str): Name of the PostgreSQL table.
+        - condition (str): Condition for deleting rows (e.g., 'column1 = value1').
+
+        Returns:
+        - None
+        """
+        conn = None
+        try:
+            # read connection parameters
+            params = self.config()
+
+            # connect to the PostgreSQL server
+            print('Connecting to the PostgreSQL database...')
+            conn = psycopg2.connect(**params)
+            
+            # create a cursor
+            cur = conn.cursor()
+            
+            # Construct the SQL query for deletion
+            query = f"DELETE FROM {table_name} WHERE {condition}"
+
+            # Execute the query
+            cur.execute(query)
+
+            # Commit the changes to the database
+            conn.commit()
+
+            print("Data deleted successfully.")
+        
+        # close the communication with the PostgreSQL
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            # Close the cursor and connection
+            if cur:
+                cur.close()
+            if conn:
+                conn.close()
+
 
 class Collector:
     def __init__(self):
