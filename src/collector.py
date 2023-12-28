0e585d1fe0c775e7f5d9603e2204f487e357278c
Ikey168 <122488906+Ikey168@users.noreply.github.com>
2023-12-28 03:26:07 +0100

    Revert "added MediaDB class with insert function"
    This reverts commit ad1b64a8b2e40f03a7f2e80b46ec5b7450f95206.

--- src/collector.py
+++ src/collector.py
@@ -1,78 +1,13 @@
-"""Collect Articles from several sources.
-Saves them into a Postgres Database
+"""This is the example module.
+
+This module does stuff.
 """
 
-from configparser import ConfigParser
-import psycopg2
-from psycopg2 import sql
-from config import config
+
+
 
 import newspaper
 
-class MediaDB:
-    
-    def __init__():
-        pass
-
-    def config(self, filename='database.ini', section='postgresql'):
-        # create a parser
-        parser = ConfigParser()
-        # read config file
-        parser.read(filename)
-
-        # get section, default to postgresql
-        db = {}
-        if parser.has_section(section):
-            params = parser.items(section)
-            for param in params:
-                db[param[0]] = param[1]
-        else:
-            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
-        return db
-
-    def insert(self, table_name, data):
-        """ Connect to the PostgreSQL database server """
-        conn = None
-        try:
-            # read connection parameters
-            params = self.config()
-
-            # connect to the PostgreSQL server
-            print('Connecting to the PostgreSQL database...')
-            conn = psycopg2.connect(**params)
-            
-            # create a cursor
-            cur = conn.cursor()
-            
-            # execute a statement
-            # Construct the SQL query for insertion
-            columns = sql.SQL(', ').join(map(sql.Identifier, data.keys()))
-            values = sql.SQL(', ').join(map(sql.Placeholder, data.values()))
-            query = sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
-                sql.Identifier(table_name),
-                columns,
-                values
-            )
-
-            # Execute the query
-            cur.execute(query, data)
-
-            # Commit the changes to the database
-            conn.commit()
-
-            print("Data inserted successfully.")
-        
-        # close the communication with the PostgreSQL
-            cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            # Close the cursor and connection
-            if cur:
-                cur.close()
-            if conn:
-                conn.close()
 
 class Collector:
     def __init__(self):
