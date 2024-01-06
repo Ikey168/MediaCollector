de6952efd3383195ef73af125de76d9f7bd4a8a2
Ikey168 <122488906+Ikey168@users.noreply.github.com>
2024-01-06 17:28:05 +0100

    NER-Test added, fixed Collector class 403 errors

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,7 +6,8 @@ sys.path.insert(1, "/home/claude/Desktop/career/Projects/MediaCollector/src")
 import collector as co
 
 col = co.Collector()
-col.article_list('https://www.foxnews.com/')
+col.article_list('https://edition.cnn.com/')
+text = col.article_texts[-1]
 
-#ner = co.NER()
-#entities = ner.extract_entitities(text)
+ner = co.NER()
+entities = ner.extract_entitities(text)
